3564ae949868f90b5e66cead8767e789ceb1cd37
Alex Jaeger <agjaeger@ualr.edu>
2015-06-19 17:18:51 -0400

    I think I added stream filter

--- cameras.py
+++ cameras.py
@@ -12,35 +12,49 @@ import sys, time
 device_ids = [2, 1]
 devices = []
 
+streams = []
+
 if __name__ == "__main__":
-		for ID in device_ids:
-			device = CameraDevice(ID)
 
-			if device.acquire_camera():
-				device.start()
-				devices.extend([device])
-			else:
-				device.release_camera()
-				del device
-				continue
+	bg_sub_stream = StreamFilter(0)
+	streams.extend([bg_sub_stream])
 
-		try:
-			while True:
-				for dev in devices:
-					frame = dev.get_frame()
-					cv2.imshow("Device: " + str(dev.get_device_id()), frame)
+	bg_sub_stream.add_operation(bg_sub_stream.bg_subtraction, algo="MOG")
 
-				if cv2.waitKey(1) & 0xFF == ord('q'):
-					break
+	stream.start()
 
-		except KeyboardInterrupt:
-			pass
+	for ID in device_ids:
+		device = CameraDevice(ID)
 
-		for device in devices:
-			device.terminate()
-			time.sleep(0.5) #wait for thread to shutdown
-
+		if device.acquire_camera():
+			device.start()
+			devices.extend([device])
+		else:
 			device.release_camera()
 			del device
+			continue
 
-		cv2.destroyAllWindows()
+	try:
+		while True:
+			for dev in devices:
+				frame = dev.get_frame()
+
+				bg_sub_stream.add_frame(frame, dev.get_device_id())
+				fg_mask = bg_sub_stream.get_frame()
+
+				cv2.imshow("Device: " + str(dev.get_device_id()), fg_mask)
+
+			if cv2.waitKey(1) & 0xFF == ord('q'):
+				break
+
+	except KeyboardInterrupt:
+		pass
+
+	for device in devices:
+		device.terminate()
+		time.sleep(0.5) #wait for thread to shutdown
+
+		device.release_camera()
+		del device
+
+	cv2.destroyAllWindows()
