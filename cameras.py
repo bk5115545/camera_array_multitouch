96b0de100279bf560c902ecbdfd79859d694dede
Alex Jaeger <agjaeger@ualr.edu>
2015-06-19 13:19:25 -0400

    Fixed formmating, spaces instead of tabs

--- cameras.py
+++ cameras.py
@@ -9,37 +9,37 @@ device_ids = [0, 1]
 devices = []
 
 if __name__ == "__main__":
-        for ID in device_ids:
-	        device = CameraDevice(ID)
+		for ID in device_ids:
+			device = CameraDevice(ID)
 
 			if device.acquire_camera():
-	        	device.start()
-	        	devices.extend([device])
-	        else:
-	                device.release_camera()
-	                del device
+				device.start()
+				devices.extend([device])
+			else:
+				device.release_camera()
+				del device
 
-        try:
-                while True:
-                	for dev in devices:
-                        frame = dev.get_frame()
+		try:
+			while True:
+				for dev in devices:
+					frame = dev.get_frame()
 
-					    if frame is not None and frame is not False:
-                            cv2.imshow('Device: ' + str(dev.get_device_id()), frame)
-                        else:
-                            print("wtf")
+					if frame is not None and frame is not False:
+						cv2.imshow('Device: ' + str(dev.get_device_id()), frame)
+					else:
+						print("wtf")
 
-                    if cv2.waitKey(1) & 0xFF == ord('q'):
-                            break
-        except KeyboardInterrupt:
-                pass
+				if cv2.waitKey(1) & 0xFF == ord('q'):
+						break
+		except KeyboardInterrupt:
+			pass
 
 
-        for device in devices:
-                device.terminate()
-                time.sleep(0.5) #wait for thread to shutdown
+		for device in devices:
+			device.terminate()
+			time.sleep(0.5) #wait for thread to shutdown
 
-                device.release_camera()
-                del device
+			device.release_camera()
+			del device
 
-        cv2.destroyAllWindows()
+		cv2.destroyAllWindows()
