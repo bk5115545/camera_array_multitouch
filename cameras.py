e211c1ec64446a91cc814246e7e92abbb110fbc0
Blaise <bbkoch@ualr.edu>
2015-06-19 16:13:51 -0500

    Changed retarded tabbing in cameras.py (probably a Windows/Linux thing). Added minor error catching in stream_filter.

--- cameras.py
+++ cameras.py
@@ -13,34 +13,34 @@ device_ids = [2, 1]
 devices = []
 
 if __name__ == "__main__":
-		for ID in device_ids:
-			device = CameraDevice(ID)
+    for ID in device_ids:
+        device = CameraDevice(ID)
 
-			if device.acquire_camera():
-				device.start()
-				devices.extend([device])
-			else:
-				device.release_camera()
-				del device
-				continue
+        if device.acquire_camera():
+            device.start()
+            devices.extend([device])
+        else:
+            device.release_camera()
+            del device
+            continue
 
-		try:
-			while True:
-				for dev in devices:
-					frame = dev.get_frame()
-					cv2.imshow("Device: " + str(dev.get_device_id()), frame)
+    try:
+        while True:
+            for dev in devices:
+                frame = dev.get_frame()
+                cv2.imshow("Device: " + str(dev.get_device_id()), frame)
 
-				if cv2.waitKey(1) & 0xFF == ord('q'):
-					break
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-		except KeyboardInterrupt:
-			pass
+    except KeyboardInterrupt:
+        pass
 
-		for device in devices:
-			device.terminate()
-			time.sleep(0.5) #wait for thread to shutdown
+    for device in devices:
+        device.terminate()
+        time.sleep(0.5) #wait for thread to shutdown
 
-			device.release_camera()
-			del device
+        device.release_camera()
+        del device
 
-		cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
